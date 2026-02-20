391245a896ee8a8b8904211671e50c10b05b9978
Pranav <pranavraipur03@gmail.com>
2026-02-20 16:36:48 +0530

    The priliminary version is working

--- main.go
+++ main.go
@@ -1,106 +1,100 @@
 package main
 
-import (
-	"bufio"
-	"fmt"
-	"os"
-	"time"
-)
-
 func main() {
 
-	fmt.Println("Please enter the following details-")
-	scanner := bufio.NewScanner(os.Stdin)
+	// fmt.Println("Please enter the following details-")
+	// scanner := bufio.NewScanner(os.Stdin)
 
-	fmt.Println("Task Name")
-	scanner.Scan()
-	inputTaskName := scanner.Text()
+	// fmt.Println("Task Name")
+	// scanner.Scan()
+	// inputTaskName := scanner.Text()
 
-	fmt.Println("Due Date")
-	scanner.Scan()
-	inputDueDate := scanner.Text()
+	// fmt.Println("Due Date")
+	// scanner.Scan()
+	// inputDueDate := scanner.Text()
 
-	fmt.Println("Specified Time on the Due Date")
-	scanner.Scan()
-	inputSpecifiedTime := scanner.Text()
-	//var createdTask tasksToBeCompleted
-	parsedDate, err := time.Parse("02-01-2006", inputDueDate)
-	if err != nil {
-		fmt.Println("The Due Date is invalid")
-	}
-	parsedTime, err := time.Parse("3:04 PM", inputSpecifiedTime)
-	if err != nil {
-		fmt.Println("The time is invalid")
-	}
+	// fmt.Println("Specified Time on the Due Date")
+	// scanner.Scan()
+	// inputSpecifiedTime := scanner.Text()
 
-	createdTask := createTask(inputTaskName, parsedDate, parsedTime)
+	// parsedDate, err := time.Parse("02-01-2006", inputDueDate)
+	// if err != nil {
+	// 	fmt.Println("The Due Date is invalid")
+	// }
+	// parsedTime, err := time.Parse("3:04 PM", inputSpecifiedTime)
+	// if err != nil {
+	// 	fmt.Println("The time is invalid")
+	// }
 
-	fmt.Printf("Reminder added successfully: %s %s %s\n", createdTask.taskName, createdTask.dueDate.Format("02-01-2006"), createdTask.specifiedTime.Format("3:04 PM"))
+	// createdTask := createTask(inputTaskName, parsedDate, parsedTime)
 
-	fmt.Println("To view the created task please type yes")
-	var confirmationToViewAgain string
-	fmt.Scanln(&confirmationToViewAgain)
-	if confirmationToViewAgain == "yes" {
-		//fmt.Printf("%s %s %s", allTasks)
-		for _, task := range allTasks {
-			fmt.Printf("%s\n", task.taskName)
-			fmt.Printf("%s\n", task.dueDate.Format("02-01-2006"))
-			fmt.Printf("%s\n", task.specifiedTime.Format("3:04 PM"))
+	// fmt.Printf("Reminder added successfully: %s %s %s\n", createdTask.taskName, createdTask.dueDate.Format("02-01-2006"), createdTask.specifiedTime.Format("3:04 PM"))
 
-		}
+	// fmt.Println("To view the created task please type yes")
+	// var confirmationToViewAgain string
+	// fmt.Scanln(&confirmationToViewAgain)
+	// if confirmationToViewAgain == "yes" {
 
-	} else {
-		fmt.Println("Thank you for your time")
-	}
-	fmt.Println("Would you like to make new reminders? Please reply with yes or no")
-	//scanner.Scan()
-	if scanner.Scan() {
-		yesOrNo := scanner.Text()
-		if yesOrNo == "yes" {
-			fmt.Println("Please enter the following details- ")
-			fmt.Println("Task Name")
-			scanner.Scan()
-			newInputTaskName := scanner.Text()
-			fmt.Println("Due Date")
-			scanner.Scan()
-			newDueDate := scanner.Text()
-			fmt.Println("Specified Time on the Due Date")
-			scanner.Scan()
-			newInputSpecifiedTime := scanner.Text()
-			newParsedDate, err := time.Parse("02-01-2006", newDueDate)
-			if err != nil {
-				fmt.Println("The Due Date is invalid")
-			}
-			newParsedTime, err := time.Parse("3:04 PM", newInputSpecifiedTime)
-			if err != nil {
-				fmt.Println("The time is invalid")
-			}
+	// 	for _, task := range allTasks {
+	// 		fmt.Printf("%s\n", task.taskName)
+	// 		fmt.Printf("%s\n", task.dueDate.Format("02-01-2006"))
+	// 		fmt.Printf("%s\n", task.specifiedTime.Format("3:04 PM"))
 
-			newCreatedTask := createTask(newInputTaskName, newParsedDate, newParsedTime)
-			fmt.Printf("Reminder added successfully: %s %s %s\n", newCreatedTask.taskName, newCreatedTask.dueDate.Format("02-01-2006"), newCreatedTask.specifiedTime.Format("3:04 PM"))
+	// 	}
 
-			fmt.Println("To view the created task please type yes")
-			var newConfirmationToViewAgain string
-			fmt.Scanln(&newConfirmationToViewAgain)
-			if newConfirmationToViewAgain == "yes" {
-				for _, newTask := range allTasks {
-					fmt.Printf("%s\n", newTask.taskName)
-					fmt.Printf("%s\n", newTask.dueDate)
-					fmt.Printf("%s\n", newTask.specifiedTime)
-				}
-			} else {
-				fmt.Println("Thank you for your time")
-			}
+	// } else {
+	// 	fmt.Println("Thank you for your time")
+	// }
+	// fmt.Println("Would you like to add new reminders? Please reply with yes or no")
 
-		} else {
-			fmt.Println("Thank you for your time")
-		}
+	// if scanner.Scan() {
+	// 	yesOrNo := scanner.Text()
+	// 	if yesOrNo == "yes" {
+	// 		fmt.Println("Please enter the following details- ")
+	// 		fmt.Println("Task Name")
+	// 		scanner.Scan()
+	// 		newInputTaskName := scanner.Text()
+	// 		fmt.Println("Due Date")
+	// 		scanner.Scan()
+	// 		newDueDate := scanner.Text()
+	// 		fmt.Println("Specified Time on the Due Date")
+	// 		scanner.Scan()
+	// 		newInputSpecifiedTime := scanner.Text()
+	// 		newParsedDate, err := time.Parse("02-01-2006", newDueDate)
+	// 		if err != nil {
+	// 			fmt.Println("The Due Date is invalid")
+	// 		}
+	// 		newParsedTime, err := time.Parse("3:04 PM", newInputSpecifiedTime)
+	// 		if err != nil {
+	// 			fmt.Println("The time is invalid")
+	// 		}
 
-	}
-	fmt.Println("Would you like to make new reminders? Please reply with yes or no")
-	scanner.Scan()
-	newYesOrNo := scanner.Text()
-	confirmation(newYesOrNo)
+	// 		newCreatedTask := createTask(newInputTaskName, newParsedDate, newParsedTime)
+	// 		fmt.Printf("Reminder added successfully: %s %s %s\n", newCreatedTask.taskName, newCreatedTask.dueDate.Format("02-01-2006"), newCreatedTask.specifiedTime.Format("3:04 PM"))
+
+	// 		fmt.Println("To view the created task please type yes")
+	// 		var newConfirmationToViewAgain string
+	// 		fmt.Scanln(&newConfirmationToViewAgain)
+	// 		if newConfirmationToViewAgain == "yes" {
+	// 			for _, newTask := range allTasks {
+	// 				fmt.Printf("%s\n", newTask.taskName)
+	// 				fmt.Printf("%s\n", newTask.dueDate)
+	// 				fmt.Printf("%s\n", newTask.specifiedTime)
+	// 			}
+	// 		} else {
+	// 			fmt.Println("Thank you for your time")
+	// 		}
+
+	// 	} else {
+	// 		fmt.Println("Thank you for your time")
+	// 	}
+
+	// }
+	// fmt.Println("Would you like to add new reminders? Please reply with yes or no")
+	// scanner.Scan()
+	// newYesOrNo := scanner.Text()
+	// confirmation(newYesOrNo)
+	grandIO()
 
 }
 
@@ -110,3 +104,5 @@ func main() {
 //there would be a grand scanner which stores the value of the very first yes then at the very end of the function with the grand scanner
 //the function will call itself recursively
 //if the user types in yes then the same loop would continue but if the user replies with no then the loop will terminate
+//20 Feb Code sprint-
+//Write the operations inside the granIO function in supporter.go
--- supporter.go
+++ supporter.go
@@ -30,51 +30,104 @@ func createTask(taskName string, dueDate time.Time, specifiedTime time.Time) tas
 //	func viewTask() {
 //		fmt.Printf("%T", allTasks)
 //	}
-func confirmation(yesOrNo string) {
-	if yesOrNo == "yes" {
-		scanner := bufio.NewScanner(os.Stdin)
-		fmt.Println("Please enter the following details- ")
-		fmt.Println("Task Name")
-		scanner.Scan()
-		newInputTaskName := scanner.Text()
-		fmt.Println("Due Date")
-		scanner.Scan()
-		newDueDate := scanner.Text()
-		fmt.Println("Specified Time on the Due Date")
-		scanner.Scan()
-		newInputSpecifiedTime := scanner.Text()
-		newParsedDate, err := time.Parse("02-01-2006", newDueDate)
-		if err != nil {
-			fmt.Println("The Due Date is invalid")
-		}
-		newParsedTime, err := time.Parse("3:04 PM", newInputSpecifiedTime)
-		if err != nil {
-			fmt.Println("The time is invalid")
-		}
+// func confirmation(yesOrNo string) {
+// 	if yesOrNo == "yes" {
+// 		scanner := bufio.NewScanner(os.Stdin)
+// 		fmt.Println("Please enter the following details- ")
+// 		fmt.Println("Task Name")
+// 		scanner.Scan()
+// 		newInputTaskName := scanner.Text()
+// 		fmt.Println("Due Date")
+// 		scanner.Scan()
+// 		newDueDate := scanner.Text()
+// 		fmt.Println("Specified Time on the Due Date")
+// 		scanner.Scan()
+// 		newInputSpecifiedTime := scanner.Text()
+// 		newParsedDate, err := time.Parse("02-01-2006", newDueDate)
+// 		if err != nil {
+// 			fmt.Println("The Due Date is invalid")
+// 		}
+// 		newParsedTime, err := time.Parse("3:04 PM", newInputSpecifiedTime)
+// 		if err != nil {
+// 			fmt.Println("The time is invalid")
+// 		}
 
-		newCreatedTask := createTask(newInputTaskName, newParsedDate, newParsedTime)
-		fmt.Printf("Reminder added successfully: %s %s %s\n", newCreatedTask.taskName, newCreatedTask.dueDate.Format("02-01-2006"), newCreatedTask.specifiedTime.Format("3:04 PM"))
+// 		newCreatedTask := createTask(newInputTaskName, newParsedDate, newParsedTime)
+// 		fmt.Printf("Reminder added successfully: %s %s %s\n", newCreatedTask.taskName, newCreatedTask.dueDate.Format("02-01-2006"), newCreatedTask.specifiedTime.Format("3:04 PM"))
 
-		fmt.Println("To view the created task please type yes")
-		var newConfirmationToViewAgain string
-		fmt.Scanln(&newConfirmationToViewAgain)
-		if newConfirmationToViewAgain == "yes" {
-			for _, newTask := range allTasks {
-				fmt.Printf("%s\n", newTask.taskName)
-				fmt.Printf("%s\n", newTask.dueDate)
-				fmt.Printf("%s\n", newTask.specifiedTime)
-			}
-		} else {
-			fmt.Println("Thank you for your time")
+// 		fmt.Println("To view the created task please type yes")
+// 		var newConfirmationToViewAgain string
+// 		fmt.Scanln(&newConfirmationToViewAgain)
+// 		if newConfirmationToViewAgain == "yes" {
+// 			for _, newTask := range allTasks {
+// 				fmt.Printf("%s\n", newTask.taskName)
+// 				fmt.Printf("%s\n", newTask.dueDate)
+// 				fmt.Printf("%s\n", newTask.specifiedTime)
+// 			}
+// 		} else {
+// 			fmt.Println("Thank you for your time")
+// 		}
+
+// 	} else {
+
+// 		fmt.Println("Thank you for your time")
+
+// 	}
+// }
+
+// func printer() {
+// 	fmt.Println("Would you like to make new reminders? Please reply with yes or no")
+// }
+
+func grandIO() {
+	fmt.Println("Please enter the following details-")
+	scanner := bufio.NewScanner(os.Stdin)
+	fmt.Println("Task Name")
+	scanner.Scan()
+	grandTaskName := scanner.Text()
+	fmt.Println("Due Date")
+	scanner.Scan()
+	grandDueDate := scanner.Text()
+	grandParsedDate, err := time.Parse("02-01-2006", grandDueDate)
+	if err != nil {
+		fmt.Println("The Due Date is invalid")
+	}
+	fmt.Println("Specified Time on the Due Date")
+	scanner.Scan()
+	grandSpecifiedTime := scanner.Text()
+	grandParsedTime, err := time.Parse("3:04 PM", grandSpecifiedTime)
+	if err != nil {
+		fmt.Println("The time is invalid")
+	}
+
+	grandCreatedTask := createTask(grandTaskName, grandParsedDate, grandParsedTime)
+	fmt.Printf("Reminder added successfully: %s %s %s\n", grandCreatedTask.taskName, grandCreatedTask.dueDate.Format("02-01-2006"), grandCreatedTask.specifiedTime.Format("3:04 PM"))
+
+	fmt.Println("To view the created task please type yes")
+	var grandConfirmationToViewAgain string
+	fmt.Scanln(&grandConfirmationToViewAgain)
+	if grandConfirmationToViewAgain == "yes" {
+		//fmt.Printf("%s %s %s", allTasks)
+		for _, task := range allTasks {
+			fmt.Printf("%s\n", task.taskName)
+			fmt.Printf("%s\n", task.dueDate.Format("02-01-2006"))
+			fmt.Printf("%s\n", task.specifiedTime.Format("3:04 PM"))
+
 		}
 
 	} else {
-
 		fmt.Println("Thank you for your time")
-
 	}
+	fmt.Println("would you like to add more reminders? Please reply with yes or no")
+	scanner.Scan()
+
+	grandYesOrNo := scanner.Text()
+	if grandYesOrNo == "yes" {
+		grandIO()
+	} else {
+		fmt.Println("Thank you for your time")
+	}
+
 }
 
-func printer() {
-	fmt.Println("Would you like to make new reminders? Please reply with yes or no")
-}
+//task.dueDate.Format("02-01-2006")) how is the execution work for this? I mean the operand evaluation
