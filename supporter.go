b2d6ba98505a8057e23af252fb45ef0af2911049
Pranav <pranavraipur03@gmail.com>
2026-02-21 18:07:05 +0530

    added json, still a few issues with storage

--- supporter.go
+++ supporter.go
@@ -2,30 +2,60 @@ package main
 
 import (
 	"bufio"
+	"encoding/json"
 	"fmt"
 	"os"
 	"text/tabwriter"
 	"time"
 )
 
-type tasksToBeCompleted struct {
-	taskName      string
-	dueDate       time.Time
-	specifiedTime time.Time
+type TasksToBeCompleted struct {
+	TaskName      string
+	DueDate       time.Time
+	SpecifiedTime time.Time
 }
 
-type Tasks []tasksToBeCompleted
+type Tasks []TasksToBeCompleted
 
 var allTasks Tasks
-var createdInside tasksToBeCompleted
+var createdInside TasksToBeCompleted
 
-func createTask(taskName string, dueDate time.Time, specifiedTime time.Time) tasksToBeCompleted {
+func createTask(taskName string, dueDate time.Time, specifiedTime time.Time) TasksToBeCompleted {
 
-	createdInside.taskName = taskName
-	createdInside.dueDate = dueDate
-	createdInside.specifiedTime = specifiedTime
+	createdInside.TaskName = taskName
+	createdInside.DueDate = dueDate
+	createdInside.SpecifiedTime = specifiedTime
 	allTasks = append(allTasks, createdInside)
 	return createdInside
+}
+
+func jsonEncoding(t Tasks) {
+
+	data := t
+	finalJson, err := json.MarshalIndent(data, "", "\t")
+	if err != nil {
+		panic(err)
+	}
+	err = os.WriteFile("storage.json", finalJson, 0644)
+	if err != nil {
+		panic(err)
+	}
+}
+
+func jsonDecoding() []TasksToBeCompleted {
+	newData, err := os.ReadFile("storage.json")
+	if err != nil {
+		panic(err)
+	}
+
+	var decodedTasks []TasksToBeCompleted
+	err = json.Unmarshal(newData, &decodedTasks)
+	if err != nil {
+		panic(err)
+	}
+
+	return decodedTasks
+
 }
 
 func grandIO() {
@@ -51,18 +81,21 @@ func grandIO() {
 	}
 
 	grandCreatedTask := createTask(grandTaskName, grandParsedDate, grandParsedTime)
-	fmt.Printf("Reminder added successfully: %s %s %s\n", grandCreatedTask.taskName, grandCreatedTask.dueDate.Format("02-01-2006"), grandCreatedTask.specifiedTime.Format("3:04 PM"))
+	fmt.Printf("Reminder added successfully: %s %s %s\n", grandCreatedTask.TaskName, grandCreatedTask.DueDate.Format("02-01-2006"), grandCreatedTask.SpecifiedTime.Format("3:04 PM"))
 
-	fmt.Println("To view the created task please type yes")
+	jsonEncoding(allTasks)
+	decodeJson := jsonDecoding()
+
+	fmt.Println("To view the created task please type Yes else No")
 	var grandConfirmationToViewAgain string
 	fmt.Scanln(&grandConfirmationToViewAgain)
 	if grandConfirmationToViewAgain == "yes" {
 		//fmt.Printf("%s %s %s", allTasks)
 		w := tabwriter.NewWriter(os.Stdout, 0, 8, 2, ' ', 0)
 		fmt.Fprintf(w, "ID\tTask\tDue Date\tTime\n")
-		for i, task := range allTasks {
+		for i, task := range decodeJson {
 
-			fmt.Fprintf(w, "%d\t%s\t%s\t%s\n", i+1, task.taskName, task.dueDate.Format("02-01-2006"), task.specifiedTime.Format("3:04 PM"))
+			fmt.Fprintf(w, "%d\t%s\t%s\t%s\n", i+1, task.TaskName, task.DueDate.Format("02-01-2006"), task.SpecifiedTime.Format("3:04 PM"))
 
 		}
 		w.Flush()
@@ -70,7 +103,7 @@ func grandIO() {
 	} else {
 		fmt.Println("Thank you for your time")
 	}
-	fmt.Println("would you like to add more reminders? Please reply with yes or no")
+	fmt.Println("would you like to add more reminders? Please reply with Yes or No")
 	scanner.Scan()
 
 	grandYesOrNo := scanner.Text()
@@ -82,4 +115,4 @@ func grandIO() {
 
 }
 
-//task.dueDate.Format("02-01-2006")) how is the execution work for this? I mean the operand evaluation
+//task.dueDate.Format("02-01-2006")) how is the execution work for this? left to right evaluation or right to left and how does it work
